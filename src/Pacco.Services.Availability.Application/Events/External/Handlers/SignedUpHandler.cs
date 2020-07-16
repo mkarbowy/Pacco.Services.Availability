6e966d73924f72d8dd42175a5755682895bf7e7b
Mikolaj Karbowy <mikolaj.karbowy@gmail.com>
2020-07-16 19:48:05 +0200

    Message broker

---
+++ src/Pacco.Services.Availability.Application/Events/External/Handlers/SignedUpHandler.cs
@@ -0,0 +1,16 @@
+﻿using Convey.CQRS.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacco.Services.Availability.Application.Events.External.Handlers
+{
+    public class SignedUpHandler : IEventHandler<SignedUp>
+    {
+        public Task HandleAsync(SignedUp @event)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/Pacco.Services.Availability.Infrastructure/Extensions.cs
+++ src/Pacco.Services.Availability.Infrastructure/Extensions.cs
@@ -1,16 +1,17 @@
 ﻿using Convey;
 using Convey.CQRS.Queries;
+using Convey.MessageBrokers.CQRS;
+using Convey.MessageBrokers.RabbitMQ;
 using Convey.Persistence.MongoDB;
 using Convey.WebApi;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Pacco.Services.Availability.Application.Events.External;
 using Pacco.Services.Availability.Core.Repositories;
 using Pacco.Services.Availability.Infrastructure.Exceptions;
 using Pacco.Services.Availability.Infrastructure.Mongo.Documents;
 using Pacco.Services.Availability.Infrastructure.Mongo.Repositories;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Pacco.Services.Availability.Infrastructure
 {
@@ -19,20 +20,24 @@ namespace Pacco.Services.Availability.Infrastructure
         public static IConveyBuilder AddInfrastructure(this IConveyBuilder builder)
         {
             builder.Services.AddTransient<IResourcesRepository, ResourcesMongoRepository>();
-            
+
             builder
                 .AddQueryHandlers()
                 .AddInMemoryQueryDispatcher()
                 .AddErrorHandler<ExceptionToResponseMapper>()
                 .AddMongo()
-                .AddMongoRepository<ResourceDocument, Guid>("resources");
+                .AddMongoRepository<ResourceDocument, Guid>("resources")
+                .AddRabbitMq();
 
             return builder;
         }
 
         public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
         {
-            app.UseErrorHandler().UseConvey();
+            app.UseErrorHandler()
+                .UseConvey()
+                .UseRabbitMq()
+                .SubscribeEvent<SignedUp>();
 
             return app;
         }
